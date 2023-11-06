b3f9f2d3c9f0c0cd094197035c922ac846e3076d
seth <seth@finsera.com>
2023-11-06 09:02:47 -0800

    Draw a little polygon.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 import sys
-import cv2
-import coco
 from pathlib import Path
+
+import cv2
 import numpy as np
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.graphics.texture import Texture
+from kivy.uix.image import Image
+
+import coco
 
 s = 0
 if len(sys.argv) > 1:
@@ -77,21 +83,39 @@ def display_objects(im: np.ndarray, objects: np.ndarray, threshold: float = 0.25
             cv2.rectangle(im, (x, y), (x + w, y + h), (255, 255, 255), 2)
 
 
-source = cv2.VideoCapture(s)
+class KivyCamera(Image):
+    def __init__(self, capture, fps, **kwargs):
+        super(KivyCamera, self).__init__(**kwargs)
+        self.capture = capture
+        Clock.schedule_interval(self.update, 1.0 / fps)
 
-win_name = "Camera Preview"
-cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+    def update(self, dt):
+        has_frame, frame = self.capture.read()
+        if has_frame:
+            objects = detect_objects(frame)
+            display_objects(frame, objects)
 
-while cv2.waitKey(1) != 27:
-    has_frame, frame = source.read()
-    if not has_frame:
-        break
+            # convert it to texture
+            buf1 = cv2.flip(frame, 0)
+            buf = buf1.tostring()
+            image_texture = Texture.create(
+                size=(frame.shape[1], frame.shape[0]), colorfmt="bgr"
+            )
+            image_texture.blit_buffer(buf, colorfmt="bgr", bufferfmt="ubyte")
+            # display image from the texture
+            self.texture = image_texture
 
-    # label = f"Shape: {frame.shape}"
-    # cv2.putText(frame, label, (0, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
-    objects = detect_objects(frame)
-    display_objects(frame, objects)
-    cv2.imshow(win_name, frame)
 
-source.release()
-cv2.destroyWindow(win_name)
+class CamApp(App):
+    def build(self):
+        self.capture = cv2.VideoCapture(s)
+        self.my_camera = KivyCamera(capture=self.capture, fps=30)
+        return self.my_camera
+
+    def on_stop(self):
+        # without this, app will not exit even if the window is closed
+        self.capture.release()
+
+
+if __name__ == "__main__":
+    CamApp().run()
