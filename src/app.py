e891df371bac4912e221d9c93809564087b3f652
seth <seth@finsera.com>
2024-03-08 16:47:49 -0800

    Detect entering polygon.

--- src/app.py
+++ src/app.py
@@ -24,9 +24,7 @@ class KittyCam(App):
 
     def build(self):
         self.capture = cv2.VideoCapture(0)
-        self.camera = Camera(
-            capture=self.capture, fps=30, size=(640, 480), class_labels=["person"]
-        )
+        self.camera = Camera(capture=self.capture, size=(640, 480), class_labels=["person"])
         self.draw_polygons = DrawPolygons(size=(640, 480))
 
         layout = FloatLayout(size=(640, 480))
@@ -42,17 +40,16 @@ class KittyCam(App):
 
     def on_start(self):
         super().on_start()
-        self.root_window.register_event_manager(
-            DrawPolygonEventManager(self.draw_polygons)
-        )
-        self.keyboard = self.root_window.request_keyboard(
-            self.keyboard_closed, self.draw_polygons
-        )
+        self.root_window.register_event_manager(DrawPolygonEventManager(self.draw_polygons))
+        self.keyboard = self.root_window.request_keyboard(self.keyboard_closed, self.draw_polygons)
         self.keyboard.bind(on_key_down=self.on_keyboard_down)
 
     def on_keyboard_down(self, _keyboard, keycode, _text, _modifiers):
         if keycode[1] == "escape":
             self.draw_polygons.clear()
+        elif keycode[1] == "tab":
+            self.camera.display = not self.camera.display
+
         return True
 
     def keyboard_closed(self):
--- src/camera.py
+++ src/camera.py
@@ -1,11 +1,15 @@
+import itertools
+import time
+from dataclasses import dataclass
 from pathlib import Path
-from typing import Collection, Set, List
+from typing import Collection, Set, List, Tuple
 
 import cv2
 import numpy as np
+import toolz
 from kivy.clock import Clock
 from kivy.graphics.texture import texture_create
-from kivy.properties import ListProperty
+from kivy.properties import ListProperty, NumericProperty, BooleanProperty
 from kivy.uix.image import Image
 from shapely import Polygon
 
@@ -25,33 +29,118 @@ net = cv2.dnn.readNetFromTensorflow(str(model_file), str(config_file))
 
 class Camera(Image):
     polygons = ListProperty([])
+    display = BooleanProperty(True)
 
     def __init__(
         self,
         capture: cv2.VideoCapture,
-        fps: int,
-        class_labels: Collection[str] | None,
-        **kwargs
+        class_labels: Collection[str] | None = None,
+        display_fps: int = 30,
+        notify_fps: int = 2,
+        debounce_seconds: int = 5,
+        **kwargs,
     ):
         super(Camera, self).__init__(**kwargs)
+        self.fps = {True: display_fps, False: notify_fps}
         self.capture = capture
         self.class_labels = set(class_labels or coco.class_labels)
-        Clock.schedule_interval(self.update, 1.0 / fps)
+        self.event = None
+        self.display = True
+        self.debounce_seconds = debounce_seconds
+        self.last_detected_time = 0.0
+        self.on_display(self, True)
+
+    def on_display(self, _instance, new_on_display):
+        if self.event:
+            self.event.cancel()
+
+        if not new_on_display:
+            has_frame, frame = self.capture.read()
+            if has_frame:
+                display_text(frame, "Running!", 10, 50)
+                self.texture = cv2_img_to_texture(frame)
+
+        self.event = Clock.schedule_interval(self.update, 1.0 / self.fps[new_on_display])
 
     def update(self, _dt):
         has_frame, frame = self.capture.read()
         if has_frame:
             objects = detect_objects(frame)
-            display_objects(frame, objects, self.class_labels, self.polygons)
-            self.texture = cv2_img_to_texture(frame)
+            class_objects = filter_objects(objects, self.class_labels, frame.shape[0], frame.shape[1])
+            do_intersect = intersect_region(class_objects, self.polygons, frame.shape[1])
+
+            if self.display:
+                display_objects(frame, class_objects, do_intersect)
+                self.texture = cv2_img_to_texture(frame)
+
+            if any(do_intersect) and ((now := time.time()) - self.last_detected_time > self.debounce_seconds):
+                for i, b in enumerate(do_intersect):
+                    if b:
+                        print(class_objects[i].label)
+                self.last_detected_time = now
 
 
 def detect_objects(im: np.ndarray, dim: int = 300) -> np.ndarray:
-    blob = cv2.dnn.blobFromImage(
-        im, 1.0, size=(dim, dim), mean=(0, 0, 0), swapRB=True, crop=False
-    )
+    blob = cv2.dnn.blobFromImage(im, 1.0, size=(dim, dim), mean=(0, 0, 0), swapRB=True, crop=False)
     net.setInput(blob)
     return net.forward()
+
+
+@dataclass
+class LabeledRectangle(object):
+    x: int
+    y: int
+    w: int
+    h: int
+    label: str
+
+
+def filter_objects(
+    objects: np.ndarray,
+    label_filter: Set[str],
+    rows: int,
+    cols: int,
+    threshold: float = 0.25,
+) -> list[LabeledRectangle]:
+    res = []
+
+    # For every Detected Object
+    for i in range(objects.shape[2]):
+        # Find the class and confidence
+        class_id = int(objects[0, 0, i, 1])
+        score = float(objects[0, 0, i, 2])
+
+        # Check if the detection is of good quality
+        class_label = coco.class_labels[class_id]
+        if score > threshold and class_label in label_filter:
+            # Recover original coordinates from normalized coordinates
+            x = int(objects[0, 0, i, 3] * cols)
+            y = int(objects[0, 0, i, 4] * rows)
+            w = int(objects[0, 0, i, 5] * cols - x)
+            h = int(objects[0, 0, i, 6] * rows - y)
+            res.append(LabeledRectangle(x, y, w, h, class_label))
+
+    return res
+
+
+def intersect_region(objects: list[LabeledRectangle], regions: list[Polygon], rows: int) -> list[bool]:
+    res = []
+    for o in objects:
+        oy = rows - o.y
+        poly = Polygon(shell=[(o.x, oy), (o.x + o.w, oy), (o.x + o.w, oy - o.h), (o.x, oy - o.h)])
+        res.append(any(poly.intersects(region) for region in regions))
+    return res
+
+
+def display_objects(im: np.ndarray, objects: list[LabeledRectangle], intersects: list[bool]):
+    for i, o in enumerate(objects):
+        if intersects[i]:
+            color = (0, 0, 255)
+        else:
+            color = (255, 255, 255)
+
+        display_text(im, o.label, o.x, o.y)
+        cv2.rectangle(im, (o.x, o.y), (o.x + o.w, o.y + o.h), color, 2)
 
 
 def display_text(im: np.ndarray, text: str, x: int, y: int):
@@ -82,48 +171,9 @@ def display_text(im: np.ndarray, text: str, x: int, y: int):
     )
 
 
-def display_objects(
-    im: np.ndarray,
-    objects: np.ndarray,
-    label_filter: Set[str],
-    polygons: List[Polygon],
-    threshold: float = 0.25,
-):
-    rows = im.shape[0]
-    cols = im.shape[1]
-
-    # For every Detected Object
-    for i in range(objects.shape[2]):
-        # Find the class and confidence
-        class_id = int(objects[0, 0, i, 1])
-        score = float(objects[0, 0, i, 2])
-
-        # Recover original coordinates from normalized coordinates
-        x = int(objects[0, 0, i, 3] * cols)
-        y = int(objects[0, 0, i, 4] * rows)
-        w = int(objects[0, 0, i, 5] * cols - x)
-        h = int(objects[0, 0, i, 6] * rows - y)
-
-        # Check if the detection is of good quality
-        if score > threshold and coco.class_labels[class_id] in label_filter:
-            display_text(im, coco.class_labels[class_id], x, y)
-            oy = rows - y
-            object_rectangle = Polygon(
-                shell=[(x, oy), (x + w, oy), (x + w, oy - h), (x, oy - h)]
-            )
-            if any(object_rectangle.intersects(polygon) for polygon in polygons):
-                color = (0, 0, 255)
-            else:
-                color = (255, 255, 255)
-
-            cv2.rectangle(im, (x, y), (x + w, y + h), color, 2)
-
-
 def cv2_img_to_texture(frame):
     buf1 = cv2.flip(frame, 0)
     buf = buf1.tostring()
-    image_texture = texture_create(
-        size=(frame.shape[1], frame.shape[0]), colorfmt="bgr"
-    )
+    image_texture = texture_create(size=(frame.shape[1], frame.shape[0]), colorfmt="bgr")
     image_texture.blit_buffer(buf, colorfmt="bgr", bufferfmt="ubyte")
     return image_texture
